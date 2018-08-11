a931b080fe47762bf3a6a6b478005eef5c885814
Kari O'Neal <triadfirewood@gmail.com>
2018-08-11 17:44:22 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  attr_reader :artist
+  @@count = 0
+  
+  @@genres = []
+  @@artist = []
+  @@genre_count = {}
+  @@artist_count = {}
+  
+  def initialize(name, artist, genre)
+   @@count +=1
+   @@genres<< genre
+   @artist = artist
+   @@artist << artist
+   @genre = genre
+   @name =name
+   
+   
+   
+   
+  
+  end
+
+  def self.count
+    
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres 
+    @@genres.uniq
+  end
+
+  def self.artist
+    @@artist.uniq
+  end
+  
+  def self.genre_count
+    @@genres.each do |genre|
+    @@genre_count[genre] = @@genres.count(genre)
+    end
+    @@genre_count
+  end
+  
+  def self.artist_count
+    @@artist.each do |artist|
+      @@artist_count[artist] = @@artist.count(artist)
+    end
+    @@artist
+  end
+end
